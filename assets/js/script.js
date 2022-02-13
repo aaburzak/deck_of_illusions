4b3d56bc3c043824e284ca35303cdaef3fb35bb7
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-02-13 18:02:54 -0500

    building out shuffle logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,42 @@
 
 const drawCard = $("#drawButton");
+const cardNameElement = $("#cardName");
+const cardImageElement = $("#cardImage");
 
-let randomCard, currentCardIndex;
-
-let deckOfCards = [
-  {
-    name: "name1",
-    pic: "image1"
-  },
-  {
-    name: "name2",
-    pic: "image2"
-  },
-  {
-    name: "name3",
-    pic: "image3"
-  },
-  {
-    name: "name4",
-    pic: "image4"
-  },
-  {
-    name: "name5",
-    pic: "image5"
-  },
-];
+let shuffleCards, currentCardIndex;
 
 drawCard.click(selectCard);
 
 function selectCard(){
-  randomCard = deckOfCards.sort(() => Math.random() -.5)
+  shuffleCards = deck.sort(()=> Math.random() -.5 )
   currentCardIndex = 0
-  // cardContainer.classList.remove('hide')
-  showCard(randomCard[currentCardIndex])
+  setNextCard ()
+  console.log("Is This your card?")
 };
 
-function showCard()
+function setNextCard (){
+  showCard(shuffleCards[currentCardIndex])
+};
+
+function showCard(deck){
+  cardNameElement.innerHtml = deck.card_Name
+  console.log(cardNameElement)
+}
+
+
+// let randomCard, currentCardIndex;
+
+const deck  = [
+{
+  card_Name:'Dragon',
+  card_Image:"assets\images\dragon.png",
+},
+{
+  card_Name:'Knight',
+  card_Image:"assets\images\knight.png",
+},
+{
+  card_Name:'Druid',
+  card_Image:"assets\images\druid.png",
+},
+];
